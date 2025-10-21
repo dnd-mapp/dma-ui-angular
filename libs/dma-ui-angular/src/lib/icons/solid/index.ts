71a33bc384685ddfd90ebef3f65776147c87a1ae
NoNamer777 <oscar.wellner@gmail.com>
2025-10-21 17:12:26 +0200

    Add ArrowUpRightFromSquare icon

--- libs/dma-ui-angular/src/lib/icons/solid/index.ts
+++ libs/dma-ui-angular/src/lib/icons/solid/index.ts
@@ -1,2 +1,3 @@
+export * from './arrow-up-right-from-square/so-arrow-up-right-from-square-icon.component';
 export * from './plus/so-plus-icon.component';
 export * from './xmark/so-xmark-icon.component';
