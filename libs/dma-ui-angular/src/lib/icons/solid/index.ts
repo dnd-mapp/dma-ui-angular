b58ba27cffe2e5f5ff5133192524ab84fd816814
NoNamer777 <oscar.wellner@gmail.com>
2025-10-21 15:43:06 +0200

    Add xmark icon

--- libs/dma-ui-angular/src/lib/icons/solid/index.ts
+++ libs/dma-ui-angular/src/lib/icons/solid/index.ts
@@ -1,1 +1,2 @@
 export * from './plus/so-plus-icon.component';
+export * from './xmark/so-xmark-icon.component';
---
+++ libs/dma-ui-angular/src/lib/icons/solid/xmark/so-xmark-icon.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-icon[dma-so-xmark-icon]',
+    templateUrl: './so-xmark-icon.component.svg',
+    styleUrl: '../../icons.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [],
+})
+export class SoXmarkIconComponent {}
