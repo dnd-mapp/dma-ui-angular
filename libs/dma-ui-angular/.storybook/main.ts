fcff81a8422b22e6ab1580bcd5d6af79a78b0af1
NoNamer777 <oscar.wellner@gmail.com>
2025-10-21 10:18:20 +0200

    Adjust Storybook's Webpack config to allow using SVGs as component templates

--- libs/dma-ui-angular/.storybook/main.ts
+++ libs/dma-ui-angular/.storybook/main.ts
@@ -1,5 +1,6 @@
 import type { StorybookConfig } from '@storybook/angular';
 import { dirname, join } from 'path';
+import { RuleSetRule } from 'webpack';
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -9,12 +10,30 @@ function getAbsolutePath(value: string) {
     return dirname(require.resolve(join(value, 'package.json')));
 }
 
+function isResourceRule(rule: unknown): rule is RuleSetRule {
+    return (
+        typeof rule === 'object' && 'type' in rule && typeof rule.type === 'string' && rule.type === 'asset/resource'
+    );
+}
+
 const config = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|mjs|ts)'],
     addons: [getAbsolutePath('@storybook/addon-docs')],
     framework: {
         name: getAbsolutePath('@storybook/angular'),
         options: {},
+    },
+    webpackFinal: (config) => {
+        const moduleRules = config.module.rules;
+
+        config.module.rules = moduleRules.map((rule) => {
+            if (!isResourceRule(rule)) return rule;
+            return {
+                ...rule,
+                test: new RegExp(rule.test.toString().replace('svg|', '')),
+            };
+        });
+        return config;
     },
 } satisfies StorybookConfig;
 
