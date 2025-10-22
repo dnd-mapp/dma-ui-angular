3ba3950d5f3816cbdf644bb4b5bb5af15d4ce6bc
NoNamer777 <oscar.wellner@gmail.com>
2025-10-22 10:13:15 +0200

    Add icons

---
+++ libs/dma-ui-angular/src/lib/icons/solid/magnifying-glass/so-magnifying-glass-icon.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-icon[dma-so-magnifying-glass-icon]',
+    templateUrl: './so-magnifying-glass-icon.component.svg',
+    styleUrl: '../../icons.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [],
+})
+export class SoMagnifyingGlassIconComponent {}
