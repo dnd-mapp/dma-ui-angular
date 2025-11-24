a00739b71568be60487af2959259119d5c8d5c61
NoNamer777 <oscar.wellner@gmail.com>
2025-11-24 11:28:43 +0100

    Add icons

---
+++ libs/dma-ui-angular/src/lib/icons/solid/chevron-down/so-chevron-down-icon.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-icon[dma-so-chevron-down-icon]',
+    templateUrl: './so-chevron-down-icon.component.svg',
+    styleUrl: '../../icons.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [],
+})
+export class SoChevronDownIconComponent {}
