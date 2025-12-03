0dece499f46081e51b81dc473df1ecce7756d72a
NoNamer777 <oscar.wellner@gmail.com>
2025-12-03 11:01:01 +0100

    Remove unused module

--- libs/dma-ui-angular/test/utils/index.ts
+++ libs/dma-ui-angular/test/utils/index.ts
@@ -1,2 +1,1 @@
 export * from './create-test-environment';
-export * from './zoneless-testing.module';
--- libs/dma-ui-angular/test/utils/zoneless-testing.module.ts
+++
@@ -1,6 +0,0 @@
-import { NgModule, provideZonelessChangeDetection } from '@angular/core';
-
-@NgModule({
-    providers: [provideZonelessChangeDetection()],
-})
-export class ZonelessTestingModule {}
