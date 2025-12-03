e4b6683459cebe2a7be5318af0cc31c1b0d6711d
NoNamer777 <oscar.wellner@gmail.com>
2025-12-03 11:17:56 +0100

    Enable zoneless testing

--- libs/dma-ui-angular/test/setup-test.ts
+++ libs/dma-ui-angular/test/setup-test.ts
@@ -3,4 +3,4 @@ import '@angular/compiler';
 import '@analogjs/vitest-angular/setup-snapshots';
 import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
 
-setupTestBed();
+setupTestBed({ zoneless: true });
