9d6ec082293f3df68fcdd35911814d413ccd8c6b
NoNamer777 <oscar.wellner@gmail.com>
2025-12-03 11:30:45 +0100

    Remove provision of zoneless change detection

--- libs/dma-ui-angular/test/setup-test.ts
+++ libs/dma-ui-angular/test/setup-test.ts
@@ -5,9 +5,4 @@ import { NgModule, provideZonelessChangeDetection } from '@angular/core';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
-@NgModule({
-    providers: [provideZonelessChangeDetection()],
-})
-class ZonelessTestModule {}
-
-getTestBed().initTestEnvironment([BrowserTestingModule, ZonelessTestModule], platformBrowserTesting());
+getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
